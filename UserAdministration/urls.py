b3c4d47338b69befe6a10cef44f8d54f4c7478b1
prashanyh <prashanth.chaduvala@arxtlabs.com>
2022-05-20 13:06:17 +0530

    added test url

--- UserAdministration/urls.py
+++ UserAdministration/urls.py
@@ -5,4 +5,6 @@ from UserAdministration import views
 urlpatterns = [
     path('user-registration/', views.RegisterApi.as_view(), name='user-registration'),
     path('user-login/', views.LoginAPIView.as_view(), name='auth-login'),
+
+    path('test_api/', views.WorkingUrls.as_view(),name='test_api'),# simple test url
 ]
