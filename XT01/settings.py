36e36cd307cebb04abac544f5b38f8e124b8a4e6
prashanyh <prashanth.chaduvala@arxtlabs.com>
2022-05-30 16:59:42 +0530

    docker removed

--- XT01/settings.py
+++ XT01/settings.py
@@ -77,12 +77,22 @@ WSGI_APPLICATION = 'XT01.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testing',
+        'USER': 'postgres',
+        'PASSWORD': 'arxt@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        }
     }
-}
 
 
 # Password validation
